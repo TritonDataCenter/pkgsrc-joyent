07b0e5a52a5e7814e16b3d7cbe2a8319410ebde7
Filip Hajny <filip@joyent.com>
2012-10-19 07:35:19 +0000

    DATASET-573 Fix multiple issues, namely the problem with the native classes that define file system flags incorrectly, resulting in files that cannot be created. Also, do not package up docs.

---
+++ hadoop/patches/patch-src_core_org_apache_hadoop_io_nativeio_NativeIO.java
@@ -0,0 +1,24 @@
+$NetBSD$
+
+--- src/core/org/apache/hadoop/io/nativeio/NativeIO.java.orig	2012-05-08 20:34:52.000000000 +0000
++++ src/core/org/apache/hadoop/io/nativeio/NativeIO.java
+@@ -37,13 +37,13 @@ public class NativeIO {
+   public static final int O_RDONLY   =    00;
+   public static final int O_WRONLY   =    01;
+   public static final int O_RDWR     =    02;
+-  public static final int O_CREAT    =  0100;
+-  public static final int O_EXCL     =  0200;
+-  public static final int O_NOCTTY   =  0400;
++  public static final int O_CREAT    =  0400;
++  public static final int O_EXCL     = 02000;
++  public static final int O_NOCTTY   = 04000;
+   public static final int O_TRUNC    = 01000;
+-  public static final int O_APPEND   = 02000;
+-  public static final int O_NONBLOCK = 04000;
+-  public static final int O_SYNC   =  010000;
++  public static final int O_APPEND   =   010;
++  public static final int O_NONBLOCK =  0200;
++  public static final int O_SYNC   =     020;
+   public static final int O_ASYNC  =  020000;
+   public static final int O_FSYNC = O_SYNC;
+   public static final int O_NDELAY = O_NONBLOCK;
