f36954d81a71edc18b8864ead40bc043e6b9a22e
Jonathan Perkin <jperkin@joyent.com>
2013-05-02 23:54:30 +0100

    Fix path and version of joyent/openjdk7.

--- openjdk7/buildlink3.mk
+++ openjdk7/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	openjdk7
 .if !defined(OPENJDK7_BUILDLINK3_MK)
 OPENJDK7_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openjdk7+=	openjdk7>=1.7.0.72.20090924
-BUILDLINK_PKGSRCDIR.openjdk7?=		../../lang/openjdk7
+BUILDLINK_API_DEPENDS.openjdk7+=	openjdk7>=1.7.0.14.14
+BUILDLINK_PKGSRCDIR.openjdk7?=		../../joyent/openjdk7
 
 .endif	# OPENJDK7_BUILDLINK3_MK
 
