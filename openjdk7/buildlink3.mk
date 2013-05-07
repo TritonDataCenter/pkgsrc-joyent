9eef4314e492040c0fe41f01508143b03b87b993
Jonathan Perkin <jperkin@joyent.com>
2013-05-07 10:36:28 +0100

    Bump version number to gain preference over lang/openjdk7.
    The OpenJDK versioning scheme is rather crazy, and the 1.7.0.147 scheme
    used by lang/openjdk7 means we now cannot use 1.7.0.<update>.<build> for
    updates.

--- openjdk7/buildlink3.mk
+++ openjdk7/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	openjdk7
 .if !defined(OPENJDK7_BUILDLINK3_MK)
 OPENJDK7_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.openjdk7+=	openjdk7>=1.7.0.14.14
+BUILDLINK_API_DEPENDS.openjdk7+=	openjdk7>=7.0.14.14
 BUILDLINK_PKGSRCDIR.openjdk7?=		../../joyent/openjdk7
 
 .endif	# OPENJDK7_BUILDLINK3_MK
