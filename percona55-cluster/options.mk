4aa059ee233fb9ce8bdcb7ed5142462fe434af70
Jonathan Perkin <jperkin@joyent.com>
2013-08-29 16:42:14 +0100

    Decouple from percona55-client, add back sphinx.
    The main server package and the cluster package are sufficiently
    different that they cannot be shared, so for now we just bundle this
    package into one and conflict with the others.

---
+++ percona55-cluster/options.mk
@@ -0,0 +1,19 @@
+# $NetBSD: options.mk,v 1.1.1.1 2011/04/25 21:12:53 adam Exp $
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.percona55-cluster
+
+# ndb-cluster does not configure with cmake
+PKG_SUPPORTED_OPTIONS+=	embedded-server ndb-cluster ssl
+PKG_SUGGESTED_OPTIONS+=	ssl
+
+.include "../../mk/bsd.options.mk"
+
+###
+### Enable OpenSSL support
+###
+.if !empty(PKG_OPTIONS:Mssl)
+.  include "../../security/openssl/buildlink3.mk"
+CMAKE_ARGS+=		-DWITH_SSL=system
+.else
+CMAKE_ARGS+=		-DWITH_SSL=no
+.endif
