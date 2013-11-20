6170ce5d21df6dc314842edbf3bfb086cfafeaf3
Filip Hajny <filip@joyent.com>
2013-11-20 09:22:47 +0000

    Add PAM support

---
+++ duo-unix/options.mk
@@ -0,0 +1,21 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=		PKG_OPTIONS.duo-unix
+PKG_SUPPORTED_OPTIONS+=		pam
+
+PKG_SUGGESTED_OPTIONS=		pam
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=	pam
+
+.if !empty(PKG_OPTIONS:Mpam)
+PLIST.pam=		yes
+CONFIGURE_ARGS+=	--with-pam=${PREFIX}/lib/security
+CONF_FILES_PERMS+=	share/examples/duo-unix/pam_duo.conf    \
+			${PKG_SYSCONFDIR}/pam_duo.conf  \
+			${REAL_ROOT_USER} ${REAL_ROOT_GROUP} 0400
+.  include "../../mk/pam.buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--without-pam
+.endif
