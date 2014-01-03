e471ef0ac37576662d0093994350ef79f5623d99
Filip Hajny <filip@joyent.com>
2014-01-03 16:53:21 +0000

    Update elasticsearch to 0.90.9

--- elasticsearch/patches/patch-bin_elasticsearch.in.sh
+++ elasticsearch/patches/patch-bin_elasticsearch.in.sh
@@ -1,19 +1,23 @@
 $NetBSD$
 
 Set default paths.
---- bin/elasticsearch.in.sh.orig	2013-11-13 12:07:38.000000000 +0000
+--- bin/elasticsearch.in.sh.orig	2013-12-23 10:36:51.000000000 +0000
 +++ bin/elasticsearch.in.sh
 @@ -1,6 +1,6 @@
  #!/bin/sh
  
--ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch-0.90.7.jar:$ES_HOME/lib/*:$ES_HOME/lib/sigar/*
+-ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch-0.90.9.jar:$ES_HOME/lib/*:$ES_HOME/lib/sigar/*
 +ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch/elasticsearch-0.90.7.jar:$ES_HOME/lib/elasticsearch/*:$ES_HOME/lib/elasticsearch/sigar/*
  
  if [ "x$ES_MIN_MEM" = "x" ]; then
      ES_MIN_MEM=256m
-@@ -62,3 +62,5 @@ JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOut
+@@ -61,4 +61,7 @@ fi
+ JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
  # The path to the heap dump location, note directory must exists and have enough
  # space for a full heap dump.
- #JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$ES_HOME/logs/heapdump.hprof"
+-#JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$ES_HOME/logs/heapdump.hprof"
+\ No newline at end of file
++#JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=$ES_HOME/logs/heapdump.hprof"
 +
++# The path to the config and pid files
 +ES_JAVA_OPTS="$ES_JAVA_OPTS -Des.config=@PKG_SYSCONFDIR@/elasticsearch.yml -Des.pidfile=@ES_WRKDIR@/elasticsearch.pid"
