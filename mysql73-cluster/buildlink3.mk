ca39949ceb0574b33cd4c304208f41d668c7dd2c
Jonathan Perkin <jperkin@joyent.com>
2014-03-20 11:26:23 +0000

    Fix mysql-cluster bl3 and sync with mysql-client.

--- mysql73-cluster/buildlink3.mk
+++ mysql73-cluster/buildlink3.mk
@@ -1,14 +1,16 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	mysql73-cluster
+BUILDLINK_TREE+=	mysql-cluster
 
-.if !defined(MYSQL73_CLUSTER_BUILDLINK3_MK)
-MYSQL73_CLUSTER_BUILDLINK3_MK:=
+.if !defined(MYSQL_CLUSTER_BUILDLINK3_MK)
+MYSQL_CLUSTER_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.mysql73-cluster+=	mysql73-cluster>=7.3.4
-BUILDLINK_PKGSRCDIR.mysql73-cluster?=	../../joyent/mysql73-cluster
-BUILDLINK_LIBDIRS.mysql73-cluster?=	lib
+BUILDLINK_API_DEPENDS.mysql-cluster+=	mysql-cluster>=7.3.4
+BUILDLINK_PKGSRCDIR.mysql-cluster?=	../../joyent/mysql73-cluster
+BUILDLINK_LIBDIRS.mysql-cluster?=	lib
 
-.endif	# MYSQL73_CLUSTER_BUILDLINK3_MK
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../security/openssl/buildlink3.mk"
+.endif	# MYSQL_CLUSTER_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-mysql73-cluster
+BUILDLINK_TREE+=	-mysql-cluster
