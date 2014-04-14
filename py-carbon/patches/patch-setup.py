142883ee4e729acd38af99c9595500a8a53bf416
Filip Hajny <filip@joyent.com>
2014-04-14 19:42:04 +0000

    Port Graphite here for <2014Q1 branches

---
+++ py-carbon/patches/patch-setup.py
@@ -0,0 +1,18 @@
+$NetBSD$
+
+Use proper paths for storage and config example files.
+--- setup.py.orig	2013-08-21 16:52:35.000000000 +0000
++++ setup.py
+@@ -13,9 +13,9 @@ else:
+   setup_kwargs = dict()
+ 
+ 
+-storage_dirs = [ ('storage/whisper',[]), ('storage/lists',[]),
+-                 ('storage/log',[]), ('storage/rrd',[]) ]
+-conf_files = [ ('conf', glob('conf/*.example')) ]
++storage_dirs = [ ('@GRAPHITE_DIR@/whisper',[]), ('@GRAPHITE_DIR@/lists',[]),
++                 ('@GRAPHITE_DIR@/log',[]), ('@GRAPHITE_DIR@/rrd',[]) ]
++conf_files = [ ('@PREFIX@/@GRAPHITE_EGDIR@', glob('conf/*.example')) ]
+ 
+ install_files = storage_dirs + conf_files
+ 
