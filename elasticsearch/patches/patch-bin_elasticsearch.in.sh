33ec2501f73cf46c57742e92d82c021eaf37d90a
Filip Hajny <filip@joyent.com>
2014-05-18 16:37:30 +0000

    Update our elasticsearch package

--- elasticsearch/patches/patch-bin_elasticsearch.in.sh
+++ elasticsearch/patches/patch-bin_elasticsearch.in.sh
@@ -1,12 +1,12 @@
 $NetBSD$
 
 Set default paths.
---- bin/elasticsearch.in.sh.orig	2014-02-25 15:54:20.000000000 +0000
+--- bin/elasticsearch.in.sh.orig	2014-04-16 14:28:49.000000000 +0000
 +++ bin/elasticsearch.in.sh
 @@ -1,6 +1,6 @@
  #!/bin/sh
  
--ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch-1.0.1.jar:$ES_HOME/lib/*:$ES_HOME/lib/sigar/*
+-ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch-1.1.1.jar:$ES_HOME/lib/*:$ES_HOME/lib/sigar/*
 +ES_CLASSPATH=$ES_CLASSPATH:$ES_HOME/lib/elasticsearch/elasticsearch-1.0.1.jar:$ES_HOME/lib/elasticsearch/*:$ES_HOME/lib/elasticsearch/sigar/*
  
  if [ "x$ES_MIN_MEM" = "x" ]; then
