68e254914dfe6bd7f80f48d16ef033466003c6b1
Filip Hajny <filip@joyent.com>
2015-02-12 08:31:43 +0000

    Update percona55 to 5.5.41.

--- percona55-client/patches/patch-client_mysqlbinlog.cc
+++ percona55-client/patches/patch-client_mysqlbinlog.cc
@@ -2,7 +2,7 @@ $NetBSD: patch-client_mysqlbinlog.cc,v 1.2 2012/10/01 15:54:40 adam Exp $
 
 * Portability: include <bstring.h> if exists.
 
---- client/mysqlbinlog.cc.orig	2012-08-29 08:50:46.000000000 +0000
+--- client/mysqlbinlog.cc.orig	2015-01-07 09:49:52.000000000 +0000
 +++ client/mysqlbinlog.cc
 @@ -38,6 +38,9 @@
  #include "sql_common.h"
@@ -12,5 +12,5 @@ $NetBSD: patch-client_mysqlbinlog.cc,v 1.2 2012/10/01 15:54:40 adam Exp $
 +#include <bstring.h> /* defines bzero() */
 +#endif
  
- #define BIN_LOG_HEADER_SIZE	4
- #define PROBE_HEADER_LEN	(EVENT_LEN_OFFSET+4)
+ /* Needed for Rpl_filter */
+ CHARSET_INFO* system_charset_info= &my_charset_utf8_general_ci;
