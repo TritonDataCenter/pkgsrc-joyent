be4f1fc4123615fa45fe3c3f12ebdf5df95ab97b
Jonathan Perkin <jperkin@joyent.com>
2016-06-30 14:20:58 +0100

    mariadb100-*: update to 10.0.26 and enable oqgraph

--- mariadb100-client/patches/patch-mysys_my__context.c
+++ mariadb100-client/patches/patch-mysys_my__context.c
@@ -2,14 +2,14 @@ $NetBSD$
 
 Don't insert DWARF directives on SunOS.
 
---- mysys/my_context.c.orig	2015-02-25 15:40:55.000000000 +0000
+--- mysys/my_context.c.orig	2016-06-23 09:24:43.000000000 +0000
 +++ mysys/my_context.c
 @@ -206,7 +206,7 @@ my_context_spawn(struct my_context *c, v
      (
       "movq %%rsp, (%[save])\n\t"
       "movq %[stack], %%rsp\n\t"
--#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4 && !defined(__INTEL_COMPILER)
-+#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4 && !defined(__INTEL_COMPILER) && !defined(__sun)
+-#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 4)) && !defined(__INTEL_COMPILER)
++#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 4)) && !defined(__INTEL_COMPILER) && !defined(__sun)
       /*
         This emits a DWARF DW_CFA_undefined directive to make the return address
         undefined. This indicates that this is the top of the stack frame, and
@@ -17,8 +17,8 @@ Don't insert DWARF directives on SunOS.
      (
       "movl %%esp, (%[save])\n\t"
       "movl %[stack], %%esp\n\t"
--#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4 && !defined(__INTEL_COMPILER)
-+#if __GNUC__ >= 4 && __GNUC_MINOR__ >= 4 && !defined(__INTEL_COMPILER) && !defined(__sun)
+-#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 4)) && !defined(__INTEL_COMPILER)
++#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 4)) && !defined(__INTEL_COMPILER) && !defined(__sun)
       /*
         This emits a DWARF DW_CFA_undefined directive to make the return address
         undefined. This indicates that this is the top of the stack frame, and
