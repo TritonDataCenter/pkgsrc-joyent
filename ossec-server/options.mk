79adbfbf7c4263bee50fd6151e7cfbb24074208d
Filip Hajny <filip@joyent.com>
2017-08-24 11:17:48 +0000

    Fix database support

--- ossec-server/options.mk
+++ ossec-server/options.mk
@@ -1,21 +1,19 @@
 # $NetBSD$
 
-PKG_OPTIONS_VAR=	PKG_OPTIONS.ossec
-PKG_SUPPORTED_OPTIONS=	mysql pgsql
-PKG_SUGGESTED_OPTIONS=	#
+PKG_OPTIONS_VAR=		PKG_OPTIONS.ossec
+PKG_SUPPORTED_OPTIONS=		#
+PKG_OPTIONS_OPTIONAL_GROUPS=	db
+PKG_OPTIONS_GROUP.db=		mysql pgsql
+PKG_SUGGESTED_OPTIONS=		#
 
 .include "../../mk/bsd.options.mk"
 
-#.if !empty(PKG_OPTIONS:Mmysql) || !empty(PKG_OPTIONS:Mpgsql)
-#BUILD_TARGET+=	setdb
-#.endif
-
 .if !empty(PKG_OPTIONS:Mmysql)
 .  include "../../mk/mysql.buildlink3.mk"
-MAKE_FLAGS+=	ENABLE_MYSQL=1
+MAKE_FLAGS+=	DATABASE=mysql
 .endif
 
 .if !empty(PKG_OPTIONS:Mpgsql)
 .  include "../../mk/pgsql.buildlink3.mk"
-MAKE_FLAGS+=	ENABLE_PGSQL=1
+MAKE_FLAGS+=	DATABASE=pgsql
 .endif
