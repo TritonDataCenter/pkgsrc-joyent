84b2a8dcd4d79fb926e5a14dd9eeb44bcc11a45d
Jonathan Perkin <jperkin@joyent.com>
2020-04-16 11:54:11 +0100

    gcc9: Revert values*.o back to GCC 7.x behaviour.

--- gcc9/patches/patch-gcc_config_sol2.h
+++ gcc9/patches/patch-gcc_config_sol2.h
@@ -1,11 +1,35 @@
 $NetBSD$
 
+Revert the values-*.o settings back to the GCC 7.x behaviour, where we avoid
+the XPG4/XPG6 values completely.  Unfortunately due to illumos#12306 there
+is widespread breakage when using them, unless users are running a platform
+newer than 20200305.  This is likely to be required for some time :(
+ 
 Add ${PREFIX}/lib to the default library search path.  These changes are
 removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
 
 --- gcc/config/sol2.h.orig	2020-03-21 10:54:58.000000000 +0000
 +++ gcc/config/sol2.h
-@@ -307,8 +307,9 @@ along with GCC; see the file COPYING3.
+@@ -199,16 +199,8 @@ along with GCC; see the file COPYING3.
+    first in ld.so.1's search path), we only link the values-*.o files into
+    executable programs.  */
+ #undef STARTFILE_ARCH_SPEC
+-#define STARTFILE_ARCH_SPEC \
+-  "%{!shared:%{!symbolic:%{!G: \
+-     %{ \
+-	std=c89|std=c90|std=gnu89|std=gnu90:values-Xa.o%s; \
+-	ansi|std=c*|std=iso9899\\:199409:values-Xc.o%s; \
+-	:values-Xa.o%s} \
+-     %{ \
+-	std=c89|std=c90|std=gnu89|std=gnu90:; \
+-	:values-xpg6.o%s \
+-     }}}}"
++#define STARTFILE_ARCH_SPEC "%{ansi:values-Xc.o%s} \
++                            %{!ansi:values-Xa.o%s}"
+ 
+ #if defined(HAVE_LD_PIE) && defined(HAVE_SOLARIS_CRTS)
+ #define STARTFILE_CRTBEGIN_SPEC "%{static:crtbegin.o%s; \
+@@ -307,8 +299,9 @@ along with GCC; see the file COPYING3.
  #define LINK_ARCH32_SPEC_BASE \
    "%{YP,*} \
     %{R*} \
@@ -17,7 +41,7 @@ removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
  
  #undef LINK_ARCH32_SPEC
  #define LINK_ARCH32_SPEC LINK_ARCH32_SPEC_BASE
-@@ -319,8 +320,9 @@ along with GCC; see the file COPYING3.
+@@ -319,8 +312,9 @@ along with GCC; see the file COPYING3.
  #define LINK_ARCH64_SPEC_BASE \
    "%{YP,*} \
     %{R*} \
