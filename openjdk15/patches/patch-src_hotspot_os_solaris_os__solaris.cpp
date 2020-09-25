dcd7310c2b234106dc9c2223e8cdafbf11da4b37
Jonathan Perkin <jperkin@joyent.com>
2020-09-25 09:32:38 +0100

    openjdk15: Fix build on PIs without EM_AARCH64.

--- openjdk15/patches/patch-src_hotspot_os_solaris_os__solaris.cpp
+++ openjdk15/patches/patch-src_hotspot_os_solaris_os__solaris.cpp
@@ -2,9 +2,9 @@ $NetBSD$
 
 Fix building on SunOS in C99 mode.
 
---- src/hotspot/os/solaris/os_solaris.cpp.orig	2019-01-08 09:40:30.000000000 +0000
+--- src/hotspot/os/solaris/os_solaris.cpp.orig	2020-05-20 18:29:11.000000000 +0000
 +++ src/hotspot/os/solaris/os_solaris.cpp
-@@ -168,6 +168,8 @@ extern "C" {
+@@ -169,6 +169,8 @@ extern "C" {
  
    static int lwp_cond_init(cond_t *cv, int scope, void *arg){ memset(cv, 0, sizeof(cond_t)); return 0; }
    static int lwp_cond_destroy(cond_t *cv)                   { return 0; }
@@ -13,7 +13,17 @@ Fix building on SunOS in C99 mode.
  }
  
  // "default" initializers for pthread-based synchronization
-@@ -2302,7 +2304,7 @@ void os::pd_commit_memory_or_exit(char*
+@@ -1430,7 +1432,9 @@ void * os::dll_load(const char *filename
+     {EM_ARM,         EM_ARM,     ELFCLASS32, ELFDATA2LSB, (char*)"ARM"},
+     // we only support 64 bit z architecture
+     {EM_S390,        EM_S390,    ELFCLASS64, ELFDATA2MSB, (char*)"IBM System/390"},
++#ifdef NOT_YET_STILL_BUILDING_ON_OLD_PLATFORMS
+     {EM_AARCH64,     EM_AARCH64, ELFCLASS64, ELFDATA2LSB, (char*)"AARCH64"}
++#endif
+   };
+ 
+ #if  (defined IA32)
+@@ -2134,7 +2138,7 @@ void os::pd_commit_memory_or_exit(char*
  
  // Uncommit the pages in a specified region.
  void os::pd_free_memory(char* addr, size_t bytes, size_t alignment_hint) {
@@ -22,7 +32,7 @@ Fix building on SunOS in C99 mode.
      debug_only(warning("MADV_FREE failed."));
      return;
    }
-@@ -2331,7 +2333,7 @@ void os::pd_realign_memory(char *addr, s
+@@ -2163,7 +2167,7 @@ void os::pd_realign_memory(char *addr, s
  // Tell the OS to make the range local to the first-touching LWP
  void os::numa_make_local(char *addr, size_t bytes, int lgrp_hint) {
    assert((intptr_t)addr % os::vm_page_size() == 0, "Address should be page-aligned.");
@@ -31,7 +41,7 @@ Fix building on SunOS in C99 mode.
      debug_only(warning("MADV_ACCESS_LWP failed."));
    }
  }
-@@ -2339,7 +2341,7 @@ void os::numa_make_local(char *addr, siz
+@@ -2171,7 +2175,7 @@ void os::numa_make_local(char *addr, siz
  // Tell the OS that this range would be accessed from different LWPs.
  void os::numa_make_global(char *addr, size_t bytes) {
    assert((intptr_t)addr % os::vm_page_size() == 0, "Address should be page-aligned.");
