fb00bc66b5c85fd7d74c16e7764fac4eaa67c659
Jonathan Perkin <jperkin@joyent.com>
2020-10-08 16:44:01 +0100

    percona80-xtrabackup: Update to 8.0.14.

--- percona80-xtrabackup/patches/patch-storage_myisam_mi__dynrec.cc
+++ percona80-xtrabackup/patches/patch-storage_myisam_mi__dynrec.cc
@@ -2,17 +2,17 @@ $NetBSD$
 
 Fix build on illumos.
 
---- storage/myisam/mi_dynrec.cc.orig	2019-12-14 16:07:34.000000000 +0000
+--- storage/myisam/mi_dynrec.cc.orig	2020-08-28 21:02:32.000000000 +0000
 +++ storage/myisam/mi_dynrec.cc
-@@ -99,7 +99,11 @@ bool mi_dynmap_file(MI_INFO *info, my_of
-     return 1;
+@@ -101,7 +101,11 @@ bool mi_dynmap_file(MI_INFO *info, my_of
+     return true;
    }
  #if defined(HAVE_MADVISE)
-+#  ifdef __sun
++# ifdef __sun
 +  posix_madvise((char *)info->s->file_map, size, MADV_RANDOM);
-+#  else
++# else
    madvise((char *)info->s->file_map, size, MADV_RANDOM);
-+#  endif
++# endif
  #endif
    info->s->mmaped_length = size;
    info->s->file_read = mi_mmap_pread;
--- percona80-xtrabackup/patches/patch-storage_myisam_mi__extra.cc
+++ percona80-xtrabackup/patches/patch-storage_myisam_mi__extra.cc
@@ -2,18 +2,19 @@ $NetBSD$
 
 Fix build on illumos.
 
---- storage/myisam/mi_extra.cc.orig	2019-12-14 16:07:34.000000000 +0000
+--- storage/myisam/mi_extra.cc.orig	2020-08-28 21:02:32.000000000 +0000
 +++ storage/myisam/mi_extra.cc
-@@ -251,8 +251,12 @@ int mi_reset(MI_INFO *info) {
+@@ -251,8 +251,13 @@ int mi_reset(MI_INFO *info) {
    if (share->base.blobs) mi_alloc_rec_buff(info, -1, &info->rec_buff);
  #if defined(HAVE_MADVISE)
    if (info->opt_flag & MEMMAP_USED)
-+#  ifdef __sun
-+    posix_madvise((char *)share->file_map, share->state.state.data_file_length, MADV_RANDOM);
-+#  else
++# ifdef __sun
++    posix_madvise((char *)share->file_map, share->state.state.data_file_length,
++            MADV_RANDOM);
++# else
      madvise((char *)share->file_map, share->state.state.data_file_length,
              MADV_RANDOM);
-+#  endif
++# endif
  #endif
    info->opt_flag &= ~(KEY_READ_USED | REMEMBER_OLD_POS);
-   info->quick_mode = 0;
+   info->quick_mode = false;
