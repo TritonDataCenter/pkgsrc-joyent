8a290829ed37aeeef335b8535008ade15f2c41a4
Jonathan Perkin <jperkin@joyent.com>
2021-03-31 13:23:23 +0100

    percona57-cluster: Fix build and tidy.

--- percona57-cluster/patches/patch-percona-xtradb-cluster-galera_galerautils_src_gu__arch.h
+++ percona57-cluster/patches/patch-percona-xtradb-cluster-galera_galerautils_src_gu__arch.h
@@ -2,9 +2,9 @@ $NetBSD$
 
 SunOS does have endian.h, but actually sys/byteorder.h is required instead.
 
---- percona-xtradb-cluster-galera/galerautils/src/gu_arch.h.orig	2017-05-31 07:19:41.000000000 +0000
+--- percona-xtradb-cluster-galera/galerautils/src/gu_arch.h.orig	2019-06-20 05:19:58.000000000 +0000
 +++ percona-xtradb-cluster-galera/galerautils/src/gu_arch.h
-@@ -9,7 +9,7 @@
+@@ -9,11 +9,11 @@
  #ifndef _gu_arch_h_
  #define _gu_arch_h_
  
@@ -13,3 +13,8 @@ SunOS does have endian.h, but actually sys/byteorder.h is required instead.
  # include <endian.h>
  #elif defined(HAVE_SYS_ENDIAN_H) /* FreeBSD */
  # include <sys/endian.h>
+-#elif defined(HAVE_SYS_BYTEORDER_H)
++#elif defined(HAVE_SYS_BYTEORDER_H) || defined(__sun)
+ # include <sys/byteorder.h>
+ #elif defined(__APPLE__)
+ # include <machine/endian.h>
