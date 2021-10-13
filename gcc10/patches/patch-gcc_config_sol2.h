62018284c5e1553957ad7fee2722273ebed60b3b
Jonathan Perkin <jperkin@joyent.com>
2021-10-13 13:15:51 +0100

    gcc10: Fix hardcoded @PREFIX@.

--- gcc10/patches/patch-gcc_config_sol2.h
+++ gcc10/patches/patch-gcc_config_sol2.h
@@ -35,9 +35,9 @@ removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
     %{R*} \
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp%R/lib:%R/usr/lib} \
 -	   %{!p:%{!pg:-Y P,%R/lib:%R/usr/lib}}}"
-+   -R/opt/local/lib \
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp%R/lib:%R/usr/lib:%R/opt/local/lib} \
-+	   %{!p:%{!pg:-Y P,%R/lib:%R/usr/lib:%R/opt/local/lib}}}"
++   -R@PREFIX@/lib \
++   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp%R/lib:%R/usr/lib:%R@PREFIX@/lib} \
++	   %{!p:%{!pg:-Y P,%R/lib:%R/usr/lib:%R@PREFIX@/lib}}}"
  
  #undef LINK_ARCH32_SPEC
  #define LINK_ARCH32_SPEC LINK_ARCH32_SPEC_BASE
@@ -47,9 +47,9 @@ removed by subst if USE_GCC_LOCALBASE_PREFIX == "no".
     %{R*} \
 -   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}	\
 -	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR "}}}"
-+   -R/opt/local/lib \
-+   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R/opt/local/lib}	\
-+	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R/opt/local/lib}}}"
++   -R@PREFIX@/lib \
++   %{!YP,*:%{p|pg:-Y P,%R/usr/lib/libp/" ARCH64_SUBDIR ":%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib}	\
++	   %{!p:%{!pg:-Y P,%R/lib/" ARCH64_SUBDIR ":%R/usr/lib/" ARCH64_SUBDIR ":%R@PREFIX@/lib}}}"
  
  #undef LINK_ARCH64_SPEC
  #ifndef USE_GLD
