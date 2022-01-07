830c19e417b231b02a794d9ff8641690f58a01a3
John Barfield <john.barfield@bissinc.com>
2022-01-07 05:12:15 +0000

    Update joyent/influxdb2 to joyent/influxdb ... 2.1.1 (cli now separete from influxd)

--- influxdb/patches/patch-.._github.com_influxdata_flux@v0.124.0_libflux_go_libflux_link__dynamic.go
+++ influxdb/patches/patch-.._github.com_influxdata_flux@v0.124.0_libflux_go_libflux_link__dynamic.go
@@ -1,6 +1,7 @@
 $NetBSD$
 
 SunOS needs socket libraries.
+cgo needs math library for pkg-config linking
 
 --- ../github.com/influxdata/flux@v0.139.0/libflux/go/libflux/link_dynamic.go.orig	1979-12-31 00:00:00.000000000 +0000
 +++ ../github.com/influxdata/flux@v0.139.0/libflux/go/libflux/link_dynamic.go
