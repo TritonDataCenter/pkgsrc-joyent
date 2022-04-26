9cb85800371cc97ddcd8ba0f6e9310dfe9ed50f2
Jonathan Perkin <jperkin@joyent.com>
2022-04-26 15:47:04 +0100

    percona80: Update to percona-server-8.0.27-18.
    Clean up a bunch of things while here.

--- percona80-client/patches/patch-storage_myisam_mi__dynrec.cc
+++ percona80-client/patches/patch-storage_myisam_mi__dynrec.cc
@@ -2,10 +2,10 @@ $NetBSD$
 
 Fix build on SunOS.
 
---- storage/myisam/mi_dynrec.cc.orig	2019-12-05 20:21:35.000000000 +0000
+--- storage/myisam/mi_dynrec.cc.orig	2022-02-28 19:55:41.000000000 +0000
 +++ storage/myisam/mi_dynrec.cc
-@@ -99,7 +99,11 @@ bool mi_dynmap_file(MI_INFO *info, my_of
-     return 1;
+@@ -101,7 +101,11 @@ bool mi_dynmap_file(MI_INFO *info, my_of
+     return true;
    }
  #if defined(HAVE_MADVISE)
 +# ifdef __sun
