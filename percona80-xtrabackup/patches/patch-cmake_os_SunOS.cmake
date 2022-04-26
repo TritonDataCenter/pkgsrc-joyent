997481c1b09a51f3d04f215e7984911b4b40d0cc
Jonathan Perkin <jperkin@joyent.com>
2022-04-26 18:53:13 +0100

    percona80-xtrabackup: Update to 8.0.28.

---
+++ percona80-xtrabackup/patches/patch-cmake_os_SunOS.cmake
@@ -0,0 +1,15 @@
+$NetBSD$
+
+Remove clearly broken fatal error.
+
+--- cmake/os/SunOS.cmake.orig	2022-04-19 18:34:03.000000000 +0000
++++ cmake/os/SunOS.cmake
+@@ -27,8 +27,6 @@ INCLUDE(CheckCXXSourceCompiles)
+ 
+ IF(CMAKE_SYSTEM_PROCESSOR MATCHES "sparc")
+   SET(SOLARIS_SPARC 1)
+-ELSE()
+-  MESSAGE(FATAL_ERROR "Solaris on Intel is not supported.")
+ ENDIF()
+ 
+ IF("${CMAKE_C_FLAGS}${CMAKE_CXX_FLAGS}" MATCHES "-m32")
