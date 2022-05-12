6487fc55ba43332dfa2386bb5b03f6cf8b9c257e
Jonathan Perkin <jperkin@joyent.com>
2022-05-12 12:59:04 +0100

    percona80-cluster: Avoid memset_s on NULL pointer.

---
+++ percona80-cluster/patches/patch-sql_binlog__reader.cc
@@ -0,0 +1,17 @@
+$NetBSD$
+
+Avoid memset_s on NULL pointer.
+
+--- sql/binlog_reader.cc.orig	2022-04-07 06:09:45.000000000 +0000
++++ sql/binlog_reader.cc
+@@ -89,7 +89,9 @@ Binlog_event_data_istream::Decryption_bu
+ }
+ 
+ bool Binlog_event_data_istream::Decryption_buffer::resize(size_t new_size) {
+-  memset_s(m_buffer, m_size, 0, m_size);
++  if (m_buffer != nullptr) {
++    memset_s(m_buffer, m_size, 0, m_size);
++  }
+   delete[] m_buffer;
+   m_size = 0;
+   m_buffer = nullptr;
