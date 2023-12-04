620dbb643d31758d2f25a4edcff183195ee7004a
Jonathan Perkin <jonathan@perkin.org.uk>
2023-12-04 13:22:13 +0000

    percona80-cluster: Update to 8.0.34.

--- percona80-cluster/patches/patch-percona-xtradb-cluster-galera_galerautils_src_gu__asio__stream__react.cpp
+++ percona80-cluster/patches/patch-percona-xtradb-cluster-galera_galerautils_src_gu__asio__stream__react.cpp
@@ -2,18 +2,21 @@ $NetBSD$
 
 Limit tcp_info to Linux and FreeBSD.
 
---- percona-xtradb-cluster-galera/galerautils/src/gu_asio_stream_react.cpp.orig	2023-09-27 15:23:42.207195182 +0000
+--- percona-xtradb-cluster-galera/galerautils/src/gu_asio_stream_react.cpp.orig	2023-12-04 11:19:57.332137528 +0000
 +++ percona-xtradb-cluster-galera/galerautils/src/gu_asio_stream_react.cpp
-@@ -272,10 +272,12 @@ size_t gu::AsioStreamReact::get_send_buf
-     return ::get_send_buffer_size(socket_);
+@@ -307,6 +307,7 @@ catch (const asio::system_error& e)
+     gu_throw_error(e.code().value()) << "error getting send buffer size";
  }
  
 +#if defined(__linux__) || defined(__FreeBSD__)
- struct tcp_info gu::AsioStreamReact::get_tcp_info()
+ struct tcp_info gu::AsioStreamReact::get_tcp_info() try
  {
      return ::get_tcp_info(socket_);
+@@ -315,6 +316,7 @@ catch (const asio::system_error& e)
+ {
+     gu_throw_error(e.code().value()) << "error getting TCP info";
  }
 +#endif
  
+ 
  void gu::AsioStreamReact::complete_client_handshake(
-     const std::shared_ptr<AsioSocketHandler>& handler,
--- percona80-cluster/patches/patch-scripts_wsrep__sst__xtrabackup-v2.sh
+++ percona80-cluster/patches/patch-scripts_wsrep__sst__xtrabackup-v2.sh
@@ -2,9 +2,9 @@ $NetBSD$
 
 Fix paths and port to SunOS.
 
---- scripts/wsrep_sst_xtrabackup-v2.sh.orig	2020-12-23 14:48:06.000000000 +0000
+--- scripts/wsrep_sst_xtrabackup-v2.sh.orig	2023-10-19 10:30:04.000000000 +0000
 +++ scripts/wsrep_sst_xtrabackup-v2.sh
-@@ -130,12 +130,12 @@ DATA="${WSREP_SST_OPT_DATA}"
+@@ -136,12 +136,12 @@ DATA="${WSREP_SST_OPT_DATA}"
  
  # default XB path (for 8.0+)  (relative to script location)
  # Use this path for 8.0+
@@ -19,7 +19,7 @@ Fix paths and port to SunOS.
  
  # These files carry some important information in form of GTID of the data
  # that is being backed up.
-@@ -153,7 +153,7 @@ PIPESTATUS_FILE="pipestatus"
+@@ -161,7 +161,7 @@ JOINER_SST_DIR=""
  
  # Setting the path for ss and ip
  # ss: is utility to investigate socket, ip for network routes.
@@ -28,7 +28,7 @@ Fix paths and port to SunOS.
  
  #-------------------------------------------------------------------------------
  #
-@@ -802,7 +802,11 @@ get_stream()
+@@ -919,7 +919,11 @@ get_stream()
  get_proc()
  {
      set +e
@@ -41,7 +41,7 @@ Fix paths and port to SunOS.
      [[ -z $nproc || $nproc -eq 0 ]] && nproc=1
      set -e
  }
-@@ -844,7 +848,7 @@ cleanup_joiner()
+@@ -964,7 +968,7 @@ cleanup_joiner()
      fi
  
      # Final cleanup
@@ -50,7 +50,7 @@ Fix paths and port to SunOS.
  
      # This means no setsid done in mysqld.
      # We don't want to kill mysqld here otherwise.
-@@ -883,7 +887,7 @@ cleanup_donor()
+@@ -1003,7 +1007,7 @@ cleanup_donor()
      fi
  
      # Final cleanup
@@ -59,7 +59,7 @@ Fix paths and port to SunOS.
  
      # This means no setsid done in mysqld.
      # We don't want to kill mysqld here otherwise.
-@@ -979,7 +983,7 @@ wait_for_listen()
+@@ -1168,7 +1172,7 @@ wait_for_listen()
          wsrep_log_debug "$LINENO: Using ss for socat/nc discovery"
  
          # Revert to using ss to check if socat/nc is listening
@@ -68,7 +68,7 @@ Fix paths and port to SunOS.
          if [[ $? -ne 0 ]]; then
              wsrep_log_error "******** FATAL ERROR *********************** "
              wsrep_log_error "* Could not find 'ss'.  Check that it is installed and in the path."
-@@ -989,7 +993,7 @@ wait_for_listen()
+@@ -1178,7 +1182,7 @@ wait_for_listen()
  
          for i in {1..300}
          do
